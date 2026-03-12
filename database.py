06da2455df2a46dc3e2291d6c8aa87a1ea85212b
Calvin C <curtindolph.calvin.10@gmail.com>
2026-03-12 14:20:18 -0500

    added Repository to database.py. added calvincTools to requirements.txt

--- database.py
+++ database.py
@@ -2,11 +2,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
+rootdir = "."
+app_dbName = f"{rootdir}/l6l10picklists.db"
+
 # Create SQLite database engine
-engine = create_engine('sqlite:///l6l10picklists.db', echo=False)
+engine = create_engine(
+    f'sqlite:///{app_dbName}', 
+    echo=False
+    )
 
 # Create all tables in the database
 Base.metadata.create_all(engine)
 
 # Create a customized Session class
 Session = sessionmaker(bind=engine)
+
+def get_app_session():
+    return Session()
+def get_app_sessionmaker():
+    return Session
+
+from calvincTools.database import Repository
