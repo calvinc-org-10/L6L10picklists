4ecab7a0a0a2427d52c4a3142cb1b07d2b85e627
Calvin C <curtindolph.calvin.10@gmail.com>
2026-03-12 22:50:05 -0500

    added cTools-required elements

---
+++ Main.py
@@ -0,0 +1,53 @@
+import sys
+from PySide6.QtWidgets import QApplication
+
+
+# the Main Screen must be in a separate file because it has to be loaded AFTER django support
+
+from PySide6.QtWidgets import (QWidget, QVBoxLayout, )
+
+
+from sysver import (_appname, sysver, )
+from calvincTools.apphooks import cTools_apphooks
+from calvincTools.cMenu import cMenu
+
+from menuformname_viewMap import FormNameToURL_Map
+from externalWebPageURL_Map import ExternalWebPageURL_Map
+from database import get_app_sessionmaker
+
+class MainScreen(QWidget):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        if not self.objectName():
+            self.setObjectName("MainWindow")
+        
+        cTools_apphooks.initialize(
+            appname=_appname,
+            sysver=sysver['DEV'],
+            FormNameToURL_Map=FormNameToURL_Map,
+            ExternalWebPageURL_Map=ExternalWebPageURL_Map,
+            app_sessionmaker=get_app_sessionmaker(),
+        )
+        
+        theMenu = cMenu(parent)
+        # theMenu.loadMenu(3, 5) #FIX cMenu!!
+        llayout = QVBoxLayout(self)
+        llayout.addWidget(theMenu)
+        
+        self.setLayout(llayout)
+        
+        self.setWindowTitle(self.tr(_appname + " " + sysver['DEV']))
+
+        # QMetaObject.connectSlotsByName(self)
+    # __init__
+# MainScreen
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    topscreen = MainScreen()
+    topscreen.show()
+
+    sys.exit(app.exec())
+    
+    
---
+++ sysver.py
@@ -0,0 +1,16 @@
+_appname='L6 to L10 sales/picking'
+_base_ver_major=0
+_base_ver_minor=0
+_base_ver_patch=1
+_ver_date='2026-02-20'
+_base_ver = f'{_base_ver_major}.{_base_ver_minor}.{_base_ver_patch}'
+sysver = {
+    'DEV': f'DEV{_base_ver}', 
+    'PROD': _base_ver,
+    'DEMO': f'DEMO{_base_ver}'
+    } 
+
+# Changelog:
+# Version - Date - Description
+# 1.0.0 - plans
+
